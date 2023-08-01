482fee9f5aa194119af661ba3f66c589e8f6c7b0
homura <preasper0+github@gmail.com>
2023-08-01 20:47:52 +0900

    reformat

--- part1/walk-the-dog/src/lib.rs
+++ part1/walk-the-dog/src/lib.rs
@@ -2,7 +2,6 @@ use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
 use web_sys::console;
 
-
 // When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
 // allocator.
 //
@@ -11,19 +10,24 @@ use web_sys::console;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
-
 // This is like the `main` function, except for JavaScript.
 #[wasm_bindgen(start)]
 pub fn main_js() -> Result<(), JsValue> {
     console_error_panic_hook::set_once();
 
-    let window = web_sys::window().unwrap(); let document = window.document().unwrap(); let canvas = document
+    let window = web_sys::window().unwrap();
+    let document = window.document().unwrap();
+    let canvas = document
         .get_element_by_id("canvas")
-        .unwrap() .dyn_into::<web_sys::HtmlCanvasElement>() .unwrap();
+        .unwrap()
+        .dyn_into::<web_sys::HtmlCanvasElement>()
+        .unwrap();
     let context = canvas
         .get_context("2d")
         .unwrap()
-        .unwrap() .dyn_into::<web_sys::CanvasRenderingContext2d>() .unwrap();
+        .unwrap()
+        .dyn_into::<web_sys::CanvasRenderingContext2d>()
+        .unwrap();
     context.move_to(300.0, 0.0); // top of triangle 上の頂点
     context.begin_path();
     context.line_to(0.0, 600.0); // bottom left of triangle
