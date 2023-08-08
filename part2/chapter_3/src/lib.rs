a3f9a702b4e1340c6c702ea5f4bfafd4d923c1d9
homura <preasper0+github@gmail.com>
2023-08-08 20:25:11 +0900

    finish 3.3

--- part2/chapter_3/src/lib.rs
+++ part2/chapter_3/src/lib.rs
@@ -1,15 +1,15 @@
 #[macro_use]
 mod browser;
 mod engine;
+mod game;
 
+use crate::engine::GameLoop;
+use crate::game::WalkTheDog;
 use rand::prelude::*;
 use serde::Deserialize;
 use std::collections::HashMap;
-use std::rc::Rc;
-use std::sync::Mutex;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
-use web_sys::console;
 
 #[derive(Deserialize)]
 struct Sheet {
@@ -31,55 +31,11 @@ struct Cell {
 #[wasm_bindgen(start)]
 pub fn main_js() -> Result<(), JsValue> {
     console_error_panic_hook::set_once();
-    let context = browser::context().expect("Could not get browser context");
-
-    wasm_bindgen_futures::spawn_local(async move {
-        let sheet: Sheet = browser::fetch_json("rhb.json")
+    browser::spawn_local(async move {
+        let game = WalkTheDog::new();
+        GameLoop::start(game)
             .await
-            .expect("Could not fetch rhb.json")
-            .into_serde()
-            .expect("Could not convert rhb.json into a Sheet structure");
-
-        let image = engine::load_image("rhb.png")
-            .await
-            .expect("Could not load rhb.png");
-
-        let mut frame = -1;
-        let interval_callback = Closure::wrap(Box::new(move || {
-            frame = (frame + 1) % 8;
-            let frame_name = format!("Run ({}).png", frame + 1);
-            let sprite = sheet.frames.get(&frame_name).expect("Cell not found");
-
-            context.clear_rect(0.0, 0.0, 600.0, 600.0);
-            context.draw_image_with_html_image_element_and_sw_and_sh_and_dx_and_dy_and_dw_and_dh(
-                &image,
-                sprite.frame.x.into(),
-                sprite.frame.y.into(),
-                sprite.frame.w.into(),
-                sprite.frame.h.into(),
-                300.0,
-                300.0,
-                sprite.frame.w.into(),
-                sprite.frame.h.into(),
-            );
-        }) as Box<dyn FnMut()>);
-
-        browser::window()
-            .unwrap()
-            .set_interval_with_callback_and_timeout_and_arguments_0(
-                interval_callback.as_ref().unchecked_ref(),
-                50,
-            );
-
-        interval_callback.forget();
+            .expect("Could not start game loop");
     });
-
     Ok(())
-}
-
-async fn fetch_json(json_path: &str) -> Result<JsValue, JsValue> {
-    let window = web_sys::window().unwrap();
-    let resp_value = wasm_bindgen_futures::JsFuture::from(window.fetch_with_str(json_path)).await?;
-    let resp: web_sys::Response = resp_value.dyn_into()?;
-    wasm_bindgen_futures::JsFuture::from(resp.json()?).await
 }
