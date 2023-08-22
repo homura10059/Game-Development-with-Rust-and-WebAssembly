6ae1a480998951eb77fe9b5865d9dd7db1772b81
homura <preasper0+github@gmail.com>
2023-08-22 20:44:17 +0900

    finish 5.6

--- part2/chapter_5/src/game.rs
+++ part2/chapter_5/src/game.rs
@@ -32,7 +32,7 @@ impl Platform {
         }
     }
 
-    fn bounding_box(&self) -> Rect {
+    fn destination_box(&self) -> Rect {
         let platform = self
             .sheet
             .frames
@@ -44,6 +44,31 @@ impl Platform {
             width: (platform.frame.w * 3).into(),
             height: platform.frame.h.into(),
         }
+    }
+
+    fn bounding_boxes(&self) -> Vec<Rect> {
+        const X_OFFSET: f32 = 60.0;
+        const END_HEIGHT: f32 = 54.0;
+        let destination_box = self.destination_box();
+        let bounding_box_one = Rect {
+            x: destination_box.x,
+            y: destination_box.y,
+            width: X_OFFSET,
+            height: END_HEIGHT,
+        };
+        let bounding_box_two = Rect {
+            x: destination_box.x + X_OFFSET,
+            y: destination_box.y,
+            width: destination_box.width - (X_OFFSET * 2.0),
+            height: destination_box.height,
+        };
+        let bounding_box_three = Rect {
+            x: destination_box.x + destination_box.width - X_OFFSET,
+            y: destination_box.y,
+            width: X_OFFSET,
+            height: END_HEIGHT,
+        };
+        vec![bounding_box_one, bounding_box_two, bounding_box_three]
     }
 
     fn draw(&self, renderer: &Renderer) {
@@ -60,8 +85,11 @@ impl Platform {
                 width: (platform.frame.w * 3).into(),
                 height: platform.frame.h.into(),
             },
-            &self.bounding_box(),
+            &self.destination_box(),
         );
+        for bounding_box in &self.bounding_boxes() {
+            renderer.draw_rect(bounding_box);
+        }
     }
 }
 
@@ -111,21 +139,23 @@ impl Game for WalkTheDog {
             }
             walk.boy.update();
 
-            if walk
-                .boy
-                .bounding_box()
-                .intersects(&walk.platform.bounding_box())
-            {
-                walk.boy.land_on(walk.platform.bounding_box().y);
+            for bounding_box in &walk.platform.bounding_boxes() {
+                if walk.boy.bounding_box().intersects(bounding_box) {
+                    if walk.boy.velocity_y() > 0 && walk.boy.pos_y() < walk.platform.position.y {
+                        walk.boy.land_on(bounding_box.y);
+                    } else {
+                        walk.boy.knock_out();
+                    }
+                }
             }
 
-            if walk
-                .boy
-                .bounding_box()
-                .intersects(walk.stone.bounding_box())
-            {
-                walk.boy.knock_out();
-            }
+            // if walk
+            //     .boy
+            //     .bounding_box()
+            //     .intersects(walk.stone.bounding_box())
+            // {
+            //     walk.boy.knock_out();
+            // }
         }
     }
     fn draw(&self, renderer: &Renderer) {
@@ -181,7 +211,7 @@ impl RedHatBoy {
         self.sprite_sheet.frames.get(&self.frame_name())
     }
 
-    fn bounding_box(&self) -> Rect {
+    fn destination_box(&self) -> Rect {
         let sprite = self.current_sprite().expect("Cell not found");
         Rect {
             x: (self.state_machine.context().position.x + sprite.sprite_source_size.x as i16)
@@ -191,6 +221,18 @@ impl RedHatBoy {
             width: sprite.frame.w.into(),
             height: sprite.frame.h.into(),
         }
+    }
+
+    fn bounding_box(&self) -> Rect {
+        const X_OFFSET: f32 = 18.0;
+        const Y_OFFSET: f32 = 14.0;
+        const WIDTH_OFFSET: f32 = 28.0;
+        let mut bounding_box = self.destination_box();
+        bounding_box.x += X_OFFSET;
+        bounding_box.width -= WIDTH_OFFSET;
+        bounding_box.y += Y_OFFSET;
+        bounding_box.height -= Y_OFFSET;
+        bounding_box
     }
 
     fn draw(&self, renderer: &Renderer) {
@@ -203,9 +245,8 @@ impl RedHatBoy {
                 width: sprite.frame.w.into(),
                 height: sprite.frame.h.into(),
             },
-            &self.bounding_box(),
+            &self.destination_box(),
         );
-        // バウンティングボックス
         renderer.draw_rect(&self.bounding_box());
     }
 
@@ -227,6 +268,14 @@ impl RedHatBoy {
 
     fn land_on(&mut self, position: f32) {
         self.state_machine = self.state_machine.transition(Event::Land(position));
+    }
+
+    fn pos_y(&self) -> i16 {
+        self.state_machine.context().position.y
+    }
+
+    fn velocity_y(&self) -> i16 {
+        self.state_machine.context().velocity.y
     }
 }
 
